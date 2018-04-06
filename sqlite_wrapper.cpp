08da898cf294dbce352ba55f3939c6bac47fafa5
Mohit Sakhuja <sakhuja.mohit@gmail.com>
2018-04-06 19:48:01 +0530

    Add basic functionality for execution of database queries

---
+++ sqlite_wrapper.cpp
@@ -0,0 +1,120 @@
+/**
+ * Contains functions for a wrapper class for sqlite3 library
+ * that contains code for connecting to a database
+ * and executing SQL queries on it.
+ *
+ * Author: Mohit Sakhuja
+ */
+
+#include "sqlite_wrapper.hpp"
+#include "helpers.hpp"
+#include <cstdio>
+#include <cstring>
+
+// open a connection to the given database
+Database::Database(const char *db_name)
+{
+    connection_status = sqlite3_open(db_name, &db);
+}
+
+// returns true is database is currently open
+bool Database::get_conn_status(void)
+{
+    return connection_status == SQLITE_OK;
+}
+
+// Execute a command on the database
+void Database::execute(const char *command, ...)
+{
+    char *formatted_cmd = new char[strlen(command) + 100];
+
+    va_list ap;
+    va_start(ap, command);
+    vsprintf(formatted_cmd, command, ap);
+
+    int return_code;
+    if (strcasestr(formatted_cmd, "max(order_id)") != NULL)
+    {
+        return_code = sqlite3_exec(db, formatted_cmd, callback_max_order_id, NULL, &errmsg);
+    }
+    else if (!strcasecmp(formatted_cmd, "select * from items;"))
+    {
+        return_code = sqlite3_exec(db, formatted_cmd, callback_add_items_to_order, NULL, &errmsg);
+    }
+    else
+    {
+        return_code = sqlite3_exec(db, formatted_cmd, callback, NULL, &errmsg);
+    }
+
+    delete[] formatted_cmd;
+    va_end(ap);
+
+    if (return_code != SQLITE_OK)
+    {
+        throw SQLITE_ERROR;
+    }
+}
+
+// Sample callback function
+int Database::callback(void *unused, int col_count, char **col_data, char **col_name)
+{
+    static int records = 0;
+    if (col_count == 0)
+    {
+        return 1;
+    }
+
+    records++;
+    printf("Row: %i\n", records);
+    for (int i = 0; i < col_count; i++)
+    {
+        printf("%s = %s\n", col_name[i], col_data[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
+// Get the last order's id
+int Database::callback_max_order_id(void *unused, int col_count, char **col_data, char **col_name)
+{
+    // Increment the last id and assign that to the new order
+    order.id = atoi(col_data[0]) + 1;
+    return 0;
+}
+
+// Adds an items to order
+int Database::callback_add_items_to_order(void *unused, int col_count, char **col_data, char **col_name)
+{
+    // Interpret data from columns
+    int id = atoi(col_data[0]);
+    string name = col_data[1];
+    float cost = atof(col_data[2]);
+    int max_quantity = atoi(col_data[3]);
+
+    // Create an item from the data
+    Item item = Item(id, name, cost, 0, max_quantity);
+
+    // Push an item into the order
+    order.items.push_back(item);
+
+    // success
+    return 0;
+}
+
+// Close the connection to the database
+Database::~Database(void)
+{
+    if (get_conn_status())
+    {
+        sqlite3_close(db);
+    }
+
+    if (errmsg != NULL)
+    {
+        sqlite3_free(errmsg);
+    }
+}
+
+Database db_order = Database("supermarket.db");
+Database db_items = Database("supermarket.db");
---
+++ sqlite_wrapper.hpp
@@ -0,0 +1,44 @@
+/**
+ * A wrapper class for sqlite3 library
+ * that contains code for connecting to a database
+ * and executing SQL queries on it.
+ *
+ * Author: Mohit Sakhuja
+ */
+
+#ifndef SQLITE_WRAPPER_HPP
+#define SQLITE_WRAPPER_HPP
+
+#include <sqlite3.h>
+
+class Database
+{
+    private:
+        int connection_status;
+        static int callback(void *unused, int col_count, char **col_data, char **col_name);
+        static int callback_max_order_id(void *unused, int col_count, char **col_data, char **col_name);
+        static int callback_add_items_to_order(void *unused, int col_count, char **col_data, char **col_name);
+
+    public:
+        // sqlite database object
+        sqlite3 *db;
+        char *errmsg;
+
+        // open a connection with the database
+        Database(char const *db_name);
+
+        // gets connection status, true if established
+        bool get_conn_status(void);
+
+        // execute SQL commands
+        void execute(const char *command, ...) __attribute__((format(__printf__, 2, 3)));
+        // int execute(const char *command, ...) __attribute__((format(__printf__, 2, 3)));
+
+        // close connection
+        ~Database(void);
+};
+
+extern Database db_order;
+extern Database db_items;
+
+#endif
